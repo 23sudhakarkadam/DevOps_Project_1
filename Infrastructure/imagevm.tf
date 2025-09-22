7cb86d275b2532a2bca3944638613b86241a518d
23sudhakarkadam <23sudhakarkadam@gmail.com>
2025-09-22 15:33:59 +0530

    added nsg

--- Infrastructure/imagevm.tf
+++ Infrastructure/imagevm.tf
@@ -67,3 +67,29 @@ resource "azurerm_linux_virtual_machine" "image_vm" {
     }
   
 }
+
+
+resource "azurerm_network_security_group" "image_nsg" {
+    name                = "image_nsg"
+    location            = azurerm_resource_group.image_rg.location  
+    resource_group_name = azurerm_resource_group.image_rg.name
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+  
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_ass" {
+    subnet_id = azurerm_subnet.image_subnet.id
+    network_security_group_id = azurerm_network_security_group.image_nsg.id
+  
+}
