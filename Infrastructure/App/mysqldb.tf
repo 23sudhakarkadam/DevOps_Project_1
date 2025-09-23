882254a325b3f3162f8a18c8673db2d9361ec34d
23sudhakarkadam <23sudhakarkadam@gmail.com>
2025-09-23 17:13:00 +0530

    created mysqldb

--- Infrastructure/App/mysqldb.tf
+++ Infrastructure/App/mysqldb.tf
@@ -4,9 +4,8 @@ resource "azurerm_mysql_flexible_server" "mysqldb" {
     location               = var.location
     administrator_login    = "mysqladmin"
     administrator_password = "MyStrongP@ssword123!"
-    sku_name               = "Standard_B1ms"
-    version                = "8.0.21"
-
+    sku_name               = "B_Standard_B2ms"
+    
         
     backup_retention_days = 7
     geo_redundant_backup_enabled = false
@@ -15,4 +14,12 @@ resource "azurerm_mysql_flexible_server" "mysqldb" {
     }
 
     delegated_subnet_id = azurerm_subnet.subnet["db_subnet"].id
+}
+
+resource "azurerm_mysql_flexible_database" "Userdb" {
+  name                = "Userdb"
+  resource_group_name = var.resource_group_name
+  server_name         = azurerm_mysql_flexible_server.mysqldb.name
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
 }
