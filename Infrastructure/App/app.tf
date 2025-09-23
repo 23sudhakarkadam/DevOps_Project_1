eea6561001b810d2ad62354fc390b4213abbe2a2
23sudhakarkadam <23sudhakarkadam@gmail.com>
2025-09-23 14:52:43 +0530

    delete scaleset

--- Infrastructure/App/app.tf
+++ Infrastructure/App/app.tf
@@ -41,34 +41,34 @@ resource "azurerm_lb_rule" "backend_rule" {
 
 
 
-resource "azurerm_linux_virtual_machine_scale_set" "app_vm" {
-    name = "app-vmss"
-    location = var.location
-    resource_group_name = var.resource_group_name
-    sku = var.vm_size
-    instances = 2
-    admin_username = var.admin_username
-    admin_password = var.admin_password
-    disable_password_authentication = false
-    zones = ["2", "3"]
+# resource "azurerm_linux_virtual_machine_scale_set" "app_vm" {
+#     name = "app-vmss"
+#     location = var.location
+#     resource_group_name = var.resource_group_name
+#     sku = var.vm_size
+#     instances = 2
+#     admin_username = var.admin_username
+#     admin_password = var.admin_password
+#     disable_password_authentication = false
+#     zones = ["2", "3"]
     
-    source_image_id = var.tomcat_image_id
-    os_disk {
-      storage_account_type = "Standard_LRS"
-      caching = "ReadWrite"
-    }
+#     source_image_id = var.tomcat_image_id
+#     os_disk {
+#       storage_account_type = "Standard_LRS"
+#       caching = "ReadWrite"
+#     }
 
-    network_interface {
-        name = "app-nic"
-        primary = true
+#     network_interface {
+#         name = "app-nic"
+#         primary = true
     
 
-        ip_configuration {
-        name = "internal"
-        primary = true
-        subnet_id = azurerm_subnet.subnet["app_subnet"].id
-        # load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.appbepool.id]
-        }
+#         ip_configuration {
+#         name = "internal"
+#         primary = true
+#         subnet_id = azurerm_subnet.subnet["app_subnet"].id
+#         # load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.appbepool.id]
+#         }
 
-}
-}
+# }
+# }
