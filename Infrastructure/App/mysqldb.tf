b8d231a6573fa5fb270f3deee39a352f22f677d6
23sudhakarkadam <23sudhakarkadam@gmail.com>
2025-09-24 10:24:03 +0530

    mysql with public access

--- Infrastructure/App/mysqldb.tf
+++ Infrastructure/App/mysqldb.tf
@@ -28,27 +28,26 @@
 #   }
 # }
 
-# resource "azurerm_mysql_flexible_server" "mysqldbskenvironmentci" {
-#     name                   = "mysqldbskenvironmentci"
-#     resource_group_name    = azurerm_resource_group.mysql_rg.name
-#     location               = azurerm_resource_group.mysql_rg.location
-#     version = "8.0.21"
-#     administrator_login    = "mysqladmin"
-#     administrator_password = "MyStrongP@ssword123!"
-#     sku_name               = "B_Standard_B2ms"
+resource "azurerm_mysql_flexible_server" "mysqldbskenvironmentci" {
+    name                   = "mysqldbskenvironmentci"
+    resource_group_name    = azurerm_resource_group.mysql_rg.name
+    location               = azurerm_resource_group.mysql_rg.location
+    version = "8.0.21"
+    administrator_login    = "mysqladmin"
+    administrator_password = "MyStrongP@ssword123!"
+    sku_name               = "B_Standard_B2ms"
     
         
-#     backup_retention_days = 7
-#     geo_redundant_backup_enabled = false
+    backup_retention_days = 7
+    geo_redundant_backup_enabled = false
+    public_network_access_enabled = true
    
+}
 
-#     delegated_subnet_id = azurerm_subnet.mysql_subnet.id
-# }
-
-# resource "azurerm_mysql_flexible_database" "Userdb" {
-#   name                = "Userdb"
-#   resource_group_name = azurerm_resource_group.mysql_rg.name
-#   server_name         = azurerm_mysql_flexible_server.mysqldbskenvironmentci.name
-#   charset             = "utf8"
-#   collation           = "utf8_unicode_ci"
-# }
+resource "azurerm_mysql_flexible_database" "Userdb" {
+  name                = "Userdb"
+  resource_group_name = azurerm_resource_group.mysql_rg.name
+  server_name         = azurerm_mysql_flexible_server.mysqldbskenvironmentci.name
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
+}
