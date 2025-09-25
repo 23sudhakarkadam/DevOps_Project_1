5cba6e75cfc142012511f0655e2a4d36eb5a050c
23sudhakarkadam <23sudhakarkadam@gmail.com>
2025-09-25 14:22:13 +0530

    create image

--- Infrastructure/image.tf
+++ Infrastructure/image.tf
@@ -1,7 +1,7 @@
-resource "azurerm_image" "example" {
+resource "azurerm_image" "image" {
     for_each = var.image_vms
     name                      = "${each.key}-image"
     location                  = var.location
-    resource_group_name       = var.location
+    resource_group_name       = var.resource_group_name
     source_virtual_machine_id = azurerm_linux_virtual_machine.image_vm[each.key].id
 }
